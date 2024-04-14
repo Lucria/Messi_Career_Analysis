342f605e63f9b9b9b188ea5376540acfd9a30018
lucria <jerryzhang28eclipse@gmail.com>
2024-04-14 11:03:39 +0800

    Clean up lesson 12

--- pl_lesson12.py
+++ pl_lesson12.py
@@ -1,89 +1,89 @@
-# import requests
+import requests
 
 # TODO display the current results for Chelsea and its position for the premier league
 # TODO Find out the scores for Chelsea vs Mancity 18 Feb
 
-# class FootballInfo:
-#     def __init__(self, api_key):
-#         self.api_key = api_key
-#         # https://www.football-data.org/documentation/quickstart/
-#         self.base_url = "https://api.football-data.org/v4/"
-#         self.headers = {"X-Auth-Token": api_key}
+class FootballInfo:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        # https://www.football-data.org/documentation/quickstart/
+        self.base_url = "https://api.football-data.org/v4/"
+        self.headers = {"X-Auth-Token": api_key}
 
-#     # 61 -> Chelsea
-#     # Retrieves information about a specific team
-#     def get_team_information(self, team_id):
-#         endpoint = f"teams/{team_id}"
+    # 61 -> Chelsea
+    # Retrieves information about a specific team
+    def get_team_information(self, team_id):
+        endpoint = f"teams/{team_id}"
 
-#         response = requests.get(f"{self.base_url}{endpoint}", headers=self.headers)
+        response = requests.get(f"{self.base_url}{endpoint}", headers=self.headers)
 
-#         if response.status_code == 200: # Success
-#             matches = response.json()
-#             print(matches)
-#         else:
-#             print(f"Error: {response.status_code}")
-
-
-#     # TODO check all the upcoming matches for Chelsea 61
-#     def get_upcoming_matches(self, team_id):
-#         print("UNDONE")
+        if response.status_code == 200: # Success
+            matches = response.json()
+            print(matches)
+        else:
+            print(f"Error: {response.status_code}")
 
 
-# api_key = input("Please input your API key:")
-# footballInfo = FootballInfo(api_key) # Create an object of the class FootballInfo
+    # TODO check all the upcoming matches for Chelsea 61
+    def get_upcoming_matches(self, team_id):
+        print("UNDONE")
 
-# footballInfo.get_team_information("61")
+
+api_key = input("Please input your API key:")
+footballInfo = FootballInfo(api_key) # Create an object of the class FootballInfo
+
+footballInfo.get_team_information("61")
 
 
 ########################################################################
 # ! Playground (Encapsulation)
 # Private (__) -> Protected (_) -> Public
 
-class BankAccount:
-  def __init__(self, balance=0, name="Adwaita", pin=873821):
-    # Private attribute (using double underscores)
-    self.__balance = balance
-    self.name = name # This is a public attribute
-    self.__pin = pin
+# class BankAccount:
+#   def __init__(self, balance=0, name="Adwaita", pin=873821):
+#     # Private attribute (using double underscores)
+#     self.__balance = balance
+#     self.name = name # This is a public attribute
+#     self.__pin = pin
 
-  def get_balance(self):
-    # Public method to access balance indirectly
-    return self.__balance
+#   def get_balance(self):
+#     # Public method to access balance indirectly
+#     return self.__balance
 
-  def deposit(self, amount):
-    # Public method to modify balance (with validation)
-    if amount > 0:
-      self.__balance += amount
-    else:
-      print("Invalid deposit amount. Must be positive.")
+#   def deposit(self, amount):
+#     # Public method to modify balance (with validation)
+#     if amount > 0:
+#       self.__balance += amount
+#     else:
+#       print("Invalid deposit amount. Must be positive.")
 
-  def withdraw(self, amount):
-    # Public method to modify balance (with validation)
-    pin = int(input("Please enter your pin number."))
-    # Check input_pin with actual pin and only allow withdrawl if pin matches, else don't allow
-    if pin == self.__pin: 
-        print("Correct pin!")
-    else:
-        print("Incorrect pin")
-        return
+#   def withdraw(self, amount):
+#     # Public method to modify balance (with validation)
+#     pin = int(input("Please enter your pin number."))
+#     # Check input_pin with actual pin and only allow withdrawl if pin matches, else don't allow
+#     if pin == self.__pin:
+#         print("Correct pin!")
+#     else:
+#         print("Incorrect pin")
+#         return
 
-    if amount <= self.__balance:
-      self.__balance -= amount
-      self.__withdraw_statement(amount)
-    else:
-      print("Insufficient funds. Withdrawal denied.")
+#     if amount <= self.__balance:
+#       self.__balance -= amount
+#       self.__withdraw_statement(amount)
+#     else:
+#       print("Insufficient funds. Withdrawal denied.")
 
-  def __withdraw_statement(self, amount):
-    # This is a private method
-    print(f"You are withdrawing {amount} dollars")
+#   def __withdraw_statement(self, amount):
+#     # This is a private method
+#     print(f"You are withdrawing {amount} dollars")
 
-account = BankAccount(100)
-print("Current Balance:", account.get_balance())
-account.deposit(50)
-account.withdraw(75)
-print("Current Balance:", account.get_balance())
+# account = BankAccount(100)
+# print("Current Balance:", account.get_balance())
+# account.deposit(50)
+# account.withdraw(75)
+# print("Current Balance:", account.get_balance())
 
-print(account.name)
+# print(account.name)
 # print(account.__balance) # This will not work
 # account.__withdraw_statement() # This will not work
 
